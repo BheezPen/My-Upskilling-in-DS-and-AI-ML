e799654123b31ce3755d3c331bd045739e538538
bheezpen <habeeblahaberejo01@gmail.com>
2024-06-29 15:13:14 +0100

    improved doc

--- generating_requirementsDOTtxt_file.py
+++ generating_requirementsDOTtxt_file.py
@@ -9,15 +9,14 @@ I found no need to create a virtual enviroment for my local system
 
 This script helps generate the requirements.txt file for your notebook (ipynb) by using pipreqsnb even when i loaded my libraries from the seerver.
 '''
-#!pip install pipreqsnb
+#!pip install pipreqsnb   # Uncommment if from server
 
-pip install pipreqsnb
 from google.colab import files
 uploaded = files.upload() #This creates an upload button, choose the notebook file and upload.
 
 
 notebook_name = 'malaria_detector_AI_model.ipynb' # Rename this value of the notebook_name variable to the name of the notebook file.
-!pipreqsnb $notebook_name
+#!pipreqsnb $notebook_name # Uncomment if from server
 
 files.download('/content/requirements.txt') # This automatically downloads the requrements.txt file
 
